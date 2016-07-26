3c8ce9c874ac52bb3c426c59b1403939db777bc0
Balachandar <dkbalachandar@gmail.com>
2016-07-26 17:19:36 -0400

    Updated with Kafka and docker Maven plugin

---
+++ Main.java
@@ -0,0 +1,33 @@
+package com;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.servlet.WebappContext;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+import javax.servlet.ServletRegistration;
+
+/**
+ * Main class.
+ *
+ * Starts a Grizzly server and deploy rest application on it
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        HttpServer server = HttpServer.createSimpleServer("/", 8080);
+        WebappContext ctx = new WebappContext("api");
+        ServletRegistration jerseyServlet = ctx.addServlet("jersey", ServletContainer.class);
+        jerseyServlet.addMapping("/api/*");
+        jerseyServlet.setInitParameter("jersey.config.server.provider.packages", "com.resource");
+        jerseyServlet.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
+        ctx.deploy(server);
+        try {
+            server.start();
+            Thread.currentThread().join();
+        } finally {
+            server.shutdownNow();
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 A Rest application send message to Kafka broker
 
 1. Clone this repository
-2. Build it and run by mvn exec:java
+2. Build it and run by mvn docker:start
 3. Call the service by accessing /api/kafka/send?msg=test data
-4. Use flume agent to validate the content. Refer the project https://github.com/dkbalachandar/flume-kafka-agent
+4. Use flume agent to validate the content. Refer the project https://github.com/dkbalachandar/flume-kafka-agent or run the below command to validate the message
+
+I assume that Docker is installed in the machine
+
+
